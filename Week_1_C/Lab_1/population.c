7b1f023efdaff5de74e8d18fc974ca3b1e2941ff
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-11-26 13:22:53 +0100

    Lab 1: Population Growth

---
+++ Week_1_C/Lab_1/population.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int s;
+    do
+    {
+        s = get_int("Start size: ");
+    }
+    while (s < 9);
+    // TODO: Prompt for end size
+    int e;
+    do
+    {
+        e = get_int("End size: ");
+    }
+    while (e < s);
+    // TODO: Calculate number of years until we reach threshold
+    int n = 0;
+    while (s < e)
+    {
+        s = s + (s / 3) - (s / 4);
+        n++;
+    }
+    // TODO: Print number of years
+    printf("Years: %i\n", n);
+}
