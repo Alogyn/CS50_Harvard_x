05a76ce3b56e40842be821e7410ab9c3bee341e3
Alogyn <mohamed.derfoufi.dev@gmail.com>
2024-01-02 14:36:16 +0100

    Week 1: C

---
+++ week_1_c/mario.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt for height '#'
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    // # Loops
+    int i, j, k;
+    for (i = 0; i < height; i++)
+    {
+        for (j = i + 1; j < height; j++)
+        {
+            printf(" ");
+        }
+        for (k = 0; k <= i; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (k = 0; k <= i; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
